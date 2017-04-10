91a92102651e1baf2fcfe099d8e8985c48775c44
Stella Kaniaru <stellakaniaru16@gmail.com>
2017-04-10 12:28:56 +0300

    solution to palindrome problem

---
+++ palindrome.py
@@ -0,0 +1,13 @@
+def palindrome(word):
+	wrd=str(word)
+	rvs=wrd[::-1]
+	print(rvs)
+	if wrd == rvs:
+	    print("This word is a palindrome")
+	else:
+	    print("This word is not a palindrome")
+
+
+if q is True:
+	s = palindrome(q)
+	print s
